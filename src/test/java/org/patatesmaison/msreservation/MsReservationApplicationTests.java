60ed81694963dbb0b84012a656cc9dbc3d71fa0e
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-07-21 09:49:52 +0200

    init++

---
+++ src/test/java/org/patatesmaison/msreservation/MsReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patatesmaison.msreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
